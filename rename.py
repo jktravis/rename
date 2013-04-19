a2f5bbd9d3b5f73d63e0667dd558d6462b417ee8
jktravis <joshua.travis@gmail.com>
2013-04-18 20:58:05 -0400

    Added options to rename files and provide extentions for batch renames

--- rename.py
+++ rename.py
@@ -1,8 +1,37 @@
-import os, argparse
+import os, argparse, sys
 
 parser = argparse.ArgumentParser(description="Remove spaces from file names and make them lowercase.")
+name_change_group = parser.add_argument_group("Name change options")
 parser.add_argument("file", nargs='+', help="The file to be renamed")
+parser.add_argument("-p", "--printonly", help="Print the output, but make no changes", action="store_true")
+name_change_group.add_argument("-n", "--new", help="Set a new file name to be used for all files. Must be used with --ext.")
+name_change_group.add_argument("-x", "--ext", help="Set the new file extension to be used for all files. Must be used with --new.")
 
-for f in args.file:
-   print "Renaming {} to {}".format(f, f.replace(" ", "_").lower() )
-   os.rename(f, f.replace(" ", "_").lower())
+args = parser.parse_args()
+
+if args.new and not args.ext:
+   sys.stderr.write("Error: Must use --ext [-e] if using --new [-n]\n")
+   parser.print_help()
+   sys.exit(1)
+
+if args.ext and not args.new:
+   sys.stderr.write("Error: Must use --new [-n] if using --ext [-e]\n")
+   parser.print_help()
+   sys.exit(1)
+
+if args.new and args.ext:
+   if args.printonly:
+      for i, f in enumerate(args.file):
+         print "Renaming {} to {}". format(f, args.new + str(i) + '.' + args.ext)
+   else:
+      for i, f in enumerate(args.file):
+         print "Renaming for realz {} to {}". format(f, args.new + str(i) + '.' + args.ext)
+         os.rename(f, args.new + str(i) + '.' + args.ext)
+else:
+   if args.printonly:
+      for f in args.file:
+         print "Renaming {} to {}".format(f, f.replace(" ", "_").lower() )
+   else:
+      for f in args.file:
+         print "Renaming for realz {} to {}".format(f, f.replace(" ", "_").lower() )
+         os.rename(f, f.replace(" ", "_").lower())
